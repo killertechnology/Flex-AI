f96843b99fec1ea405edc447d7b738dd34f77306
Kevin Fessler <killertechnology@gmail.com>
2025-01-17 08:18:20 -0800

    white theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,18 +15,20 @@ function Navbar() {
                 <div className="flex-logo"><Link to="/">Flex-AI</Link>  </div> 
                 <div></div>
             </div>
-            <div className="hamburger" onClick={toggleMenu}>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
-                <Link to="/services2">Services</Link>
-                <Link to="/careers">Careers</Link>
-                <Link to="/contact">Contact Us</Link>
-            </div>
+                <div id='navburger' className='navburger'>
+                    <div className="hamburger" onClick={toggleMenu}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className={`nav-links ${isOpen ? 'active' : ''} hamburgerbar`}>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About Us</Link>
+                        <Link to="/services2">Services</Link>
+                        <Link to="/careers">Careers</Link>
+                        <Link to="/contact">Contact Us</Link>
+                    </div>
+                </div>
         </nav>
     );
 }
